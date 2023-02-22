bf9f2303e399994f6d98807b01208cd4463f4197
VladyslavaDvorovenko <chempion0308@gmail.com>
2023-02-22 23:27:06 +0200

    zd 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryEl = document.querySelector('.gallery');
+function createCardMarkup(array) {
+  return array
+    .map(
+      item =>
+        `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`
+    )
+    .join('');
+}
 
+const CardMarkup = createCardMarkup(galleryItems);
+galleryEl.insertAdjacentHTML('beforeend', CardMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
 console.log(galleryItems);
