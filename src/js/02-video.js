d36ca82b1b3e1c8cea13514155665585dbeede6d
VladyslavaDvorovenko <chempion0308@gmail.com>
2023-02-23 17:50:25 +0200

    Дз 8

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const onPlay = function (data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+};
+player.on('timeupdate', onPlay);
+console.log(localStorage.getItem('videoplayer-current-time'));
+player
+  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
+import throttle from 'lodash.throttle';
 
+const contactFormEl = document.querySelector('.feedback-form');
+const userInfo = {};
+
+const fillContactFormFields = () => {
+  try {
+    const userInfoFormLS = JSON.parse(
+      localStorage.getItem('feedback-form-state')
+    );
+
+    if (userInfoFormLS === null) {
+      return;
+    }
+
+    for (const prop in userInfoFormLS) {
+      contactFormEl.elements[prop].value = userInfoFormLS[prop];
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+fillContactFormFields();
+
+const onContactFormFieldInput = event => {
+  const email = contactFormEl.elements.email.value;
+  const message = contactFormEl.elements.message.value;
+  const userInfo = {
+    email,
+    message,
+  };
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(userInfo));
+};
+
+const onContactFormFieldSubmit = event => {
+  event.preventDefault();
+  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+
+  contactFormEl.reset();
+  localStorage.removeItem('feedback-form-state');
+};
+
+contactFormEl.addEventListener('input', throttle(onContactFormFieldInput, 500));
+contactFormEl.addEventListener('submit', onContactFormFieldSubmit);
